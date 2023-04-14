080bdc074bd1762aa60e406a79002d46d2486c58
Philip de Lang <p_de_lang@hotmail.com>
2023-04-14 12:25:15 +0200

    Add files via upload

--- weblogic/run.sh
+++ weblogic/run.sh
@@ -16,12 +16,12 @@ function waitForContainer {
 }
 
 echo Starting MySQL container
-if ! docker run --name opa-mysql-full -e MYSQL_ROOT_PASSWORD=Passw0rd --health-cmd="mysqladmin ping --silent" -d mysql:5.7.21; then
+if ! docker run --name opa-mysql-full -e MYSQL_ROOT_PASSWORD=Passw0rd -e MYSQL_ALLOW_EMPTY_PASSWORD=true -p 3306:3306 -e  MYSQL_USER=opa -e  MYSQL_PASSWORD=Passw0rd1 -v /share/CACHEDEV1_DATA/AppSettings/mysql/conf.d:/etc/mysql/conf.d --health-cmd="mysqladmin --password=Passw0rd ping --silent" -d mysql:5.7.41; then
     exit 1
 fi
 
 echo Starting Weblogic container
-if ! docker run --name opa-weblogic-full -v ${pwd}/domain.properties:/u01/oracle/properties/domain.properties -p 7001:7001 -e ADMINISTRATION_PORT_ENABLED=false --link opa-mysql-full:opa-mysql-full --health-cmd="curl -f http://localhost:7001/console || exit 1" -d container-registry.oracle.com/middleware/weblogic:12.2.1.3; then
+if ! docker run --name opa-weblogic-full -v /share/CACHEDEV1_DATA/AppSettings/weblogic/properties:/u01/oracle/properties -p 7001:7001 -e ADMINISTRATION_PORT_ENABLED=false --link opa-mysql-full:opa-mysql-full --health-cmd="curl -f http://10.0.1.100:7001/console || exit 1" -d container-registry.oracle.com/middleware/weblogic:12.2.1.4; then
     exit 1
 fi
 
@@ -31,13 +31,15 @@ echo All containers running
 
 echo Installing OPA
 if ! ( docker cp opa opa-weblogic-full:/u01/oracle/opa \
-    && docker exec --privileged --user root opa-weblogic-full chown oracle:oracle -R /u01/oracle/opa \
+    && docker exec --privileged --user root opa-weblogic-full chown 1000:0 -R /u01/oracle/opa \
     && docker exec --user root opa-weblogic-full chmod +x -R /u01/oracle/opa/bin/install.sh \
-    && docker exec opa-weblogic-full /u01/oracle/opa/bin/install.sh install -non-secure-cookie=true -name=dev -dbconn=opa-mysql-full:3306 -dbuser=root -dbpass=Passw0rd -hubpass=Passw0rd -key=12345678 -wladmin=AdminServer -wladminurl=t3://localhost:7001 -target=AdminServer -wldomain=/u01/oracle/user_projects/domains/base_domain -wlstdir=/u01/oracle/wlserver/common/bin ); then
+    && docker exec opa-weblogic-full /u01/oracle/opa/bin/install.sh install -non-secure-cookie -no-ssl -name=dev -dbconn=opa-mysql-full:3306 -dbuser=root -dbpass=Passw0rd -hubpass=Passw0rd -key=12345678 -wladmin=AdminServer -wladminurl=t3://localhost:7001 -target=AdminServer -wldomain=/u01/oracle/user_projects/domains/base_domain -wlstdir=/u01/oracle/wlserver/common/bin ); then
     exit 1
 fi
 
 echo Your hub is now ready at http://localhost:7001/dev/opa-hub
 echo WARNING: Take note of the generated admin password and encryption key in the log above.
+
+# docker cp opa-weblogic-full:/u01/oracle/opa/bin/install.log ./
 
 # docker exec -i -t opa-weblogic-full /bin/bash
